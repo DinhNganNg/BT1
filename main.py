3d67baa6fe0e349baa42666564258e8d02eedf7c
DinhNganNg <113299182+DinhNganNg@users.noreply.github.com>
2023-11-29 09:01:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import pandas as pd
 
 st.title("Data ")
 
+st.header("Upload data file")
 data_file = st.file_uploader('Choose a csv file', type=(['.csv']))
 
 if data_file is not None:
   df = pd.read_csv(data_file)
-  
+  st.header("Show data")
   st.dataframe(df)
