2d08daf8cf83f9ff637b59e0633ab4c1cd50d758
DinhNganNg <113299182+DinhNganNg@users.noreply.github.com>
2023-11-29 08:55:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Data ")
 
-#st.file_uploader
+data_file = st.file_uploader('Choose a csv file', type=(['.csv']))
+
+if data_file is not None:
+  df = pd.read_csv(data_file)
+  
+  st.dataframe(df)
